750e3e8c56c7c6105ad0825db06837c48368ab5d
長町俊吾 <nagamaaaaachi@gmail.com>
2019-11-23 17:37:56 +0900

    ~fix JSONデータからテーブル要素を表示できた、あざす

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import axios from 'axios'
-import Button from '@material-ui/core/Button'
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -14,66 +14,69 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
 class App extends React.Component {
-  render() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			rows: [],
+		};
+	}
 
-    let rows = []
+	handleClick = () => {
+		axios
+			.get('http://localhost:3000/api/home', {
+				headers: {
+					Accept: 'Application/json',
+				},
+			})
+			.then(res => {
+				this.setState({ rows: res.data });
+			})
+			.catch(console.error);
+	};
 
-    const handleClick = () => {
-      axios.get("http://localhost:3000/api/home", {
-        headers: {
-          "Accept": "Application/json"
-        }
-      })
-        .then((res) => {
-          console.log(res);
-          rows = res.data
-          console.log(rows)
+	render() {
+		const useStyles = makeStyles({
+			root: {
+				width: '100%',
+				overflowX: 'auto',
+			},
+			table: {
+				minWidth: 650,
+			},
+		});
 
-        })
-        .catch(console.error);
-    }
+		// const classes = useStyles();
 
-    const useStyles = makeStyles({
-      root: {
-        width: '100%',
-        overflowX: 'auto',
-      },
-      table: {
-        minWidth: 650,
-      },
-    });
-
-    const classes = useStyles();
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
-          <Button variant="contained" color="primary" onClick={handleClick}>Default</Button>
-          <Paper className={classes.root}>
-            <Table className={classes.table} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>アルバム</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row, index) => (
-                  // <TableRow key={index}>
-                  //   <TableCell>{row.Album}</TableCell>
-                  // </TableRow>
-                  row
-                ))}
-              </TableBody>
-            </Table>
-          </Paper>
-
-        </header>
-      </div>
-    );
-  }
-
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<Button variant="contained" color="primary" onClick={this.handleClick}>
+						Default
+					</Button>
+					<Paper className={useStyles.root}>
+						<Table className={useStyles.table} aria-label="simple table">
+							<TableHead>
+								<TableRow>
+									<TableCell>アルバム</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{this.state.rows.map((row, index) => (
+									<TableRow key={index}>
+										<TableCell>{row.Album}</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</Paper>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
