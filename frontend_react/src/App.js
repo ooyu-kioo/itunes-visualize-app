c55a08df9806fe8c53936291163d8594971b41af
長町俊吾 <nagamaaaaachi@gmail.com>
2019-11-23 20:32:43 +0900

    ~fix Router周りのFIX # URLに応じてコンポーネント表示を切り替えるように修正 # ヘッダーは一旦コメントアウト

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,79 +1,30 @@
 import React from 'react';
+// react-router
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// Components
+import TopPage from './TopPage';
+import Visualize from './Visualize';
+// このページの関連
 import logo from './logo.svg';
 import './App.css';
 
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
-
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			rows: [],
-		};
-	}
-
-	handleClick = () => {
-		axios
-			.get('http://localhost:3000/api/home', {
-				headers: {
-					Accept: 'Application/json',
-				},
-			})
-			.then(res => {
-				this.setState({ rows: res.data });
-			})
-			.catch(console.error);
-	};
-
 	render() {
-		const useStyles = makeStyles({
-			root: {
-				width: '100%',
-				overflowX: 'auto',
-			},
-			table: {
-				minWidth: 650,
-			},
-		});
-
-		// const classes = useStyles();
-
 		return (
-			<div className="App">
-				<header className="App-header">
+			<div>
+				{/* <header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 					<p>
 						Edit <code>src/App.js</code> and save to reload.
 					</p>
-					<Button variant="contained" color="primary" onClick={this.handleClick}>
-						Default
-					</Button>
-					<Paper className={useStyles.root}>
-						<Table className={useStyles.table} aria-label="simple table">
-							<TableHead>
-								<TableRow>
-									<TableCell>アルバム</TableCell>
-								</TableRow>
-							</TableHead>
-							<TableBody>
-								{this.state.rows.map((row, index) => (
-									<TableRow key={index}>
-										<TableCell>{row.Album}</TableCell>
-									</TableRow>
-								))}
-							</TableBody>
-						</Table>
-					</Paper>
-				</header>
+				</header> */}
+				{/* URLに応じてコンポーネント表示を切り替え */}
+				<Router>
+					<div>
+						<Route path="/top" component={TopPage} />
+						<Route path="/visual" component={Visualize} />
+					</div>
+				</Router>
 			</div>
 		);
 	}
--- frontend_react/src/Visualize.js
+++ frontend_react/src/Visualize.js
@@ -1,5 +1,71 @@
 import React from 'react';
+import axios from 'axios';
 
-class Visualize extends React.Component {}
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+class Visualize extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			rows: [],
+		};
+	}
+
+	handleClick = () => {
+		axios
+			.get('http://localhost:3000/api/home', {
+				headers: {
+					Accept: 'Application/json',
+				},
+			})
+			.then(res => {
+				this.setState({ rows: res.data });
+			})
+			.catch(console.error);
+	};
+
+	render() {
+		const useStyles = makeStyles({
+			root: {
+				width: '100%',
+				overflowX: 'auto',
+			},
+			table: {
+				minWidth: 650,
+			},
+		});
+
+		return (
+			<div>
+				<Button variant="contained" color="primary" onClick={this.handleClick}>
+					Default
+				</Button>
+				<Paper className={useStyles.root}>
+					<Table className={useStyles.table} aria-label="simple table">
+						<TableHead>
+							<TableRow>
+								<TableCell>アルバム</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{this.state.rows.map((row, index) => (
+								<TableRow key={index}>
+									<TableCell>{row.Album}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</Paper>
+			</div>
+		);
+	}
+}
 
 export default Visualize;
