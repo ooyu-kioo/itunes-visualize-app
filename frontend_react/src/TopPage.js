2c1133604b0f3d938d8754fd03840f3b36cb5815
長町俊吾 <nagamaaaaachi@gmail.com>
2019-11-23 20:22:04 +0900

    ~add `react-router-dom`を追加

---
+++ frontend_react/src/TopPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+class TopPage extends React.Component {}
+
+export default TopPage;
---
+++ frontend_react/src/Visualize.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+class Visualize extends React.Component {}
+
+export default Visualize;
